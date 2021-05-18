a9440be33d48c9f6d532dcb436370afb50fbdde5
HaopengSun <sunhaopeng2016@gmail.com>
2021-05-18 11:03:53 -0400

    initial react file

---
+++ hook-practice/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UseReduce from './components/UseReducer'
+
+function App() {
+  return (
+    <div className="App">
+      <UseReduce />
+    </div>
+  );
+}
+
+export default App;
---
+++ hook-practice/src/components/UseReducer.jsx
@@ -0,0 +1,9 @@
+import React, { useReducer, useState } from 'react'
+
+const UseReducer = function (){
+  return(
+    <div></div>
+  )
+}
+
+export default UseReducer;
